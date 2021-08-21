922596e97e906198d15f1bfd95e7ecd79dfcac7d
André Kody Fernandes <im@kdy.ch>
2021-08-22 00:13:01 +0200

    Upgrades and use a reaction instead

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # EarRapeLawyer
- Discord bot detecting videos with ear rape.
+Discord bot detecting videos with ear rape.
+
+Original code from [ObiArt/EarRapeLawyer](https://github.com/ObiArt/EarRapeLawyer)
+
+## Install
+- Copy `config.json.dist` to `config.json` and edit it
+- `yarn install`
+- `node index.js`
+
+## Config
+- botToken : Your bot token [(guide)](https://discordjs.guide/preparations/setting-up-a-bot-application.html)
+- maxFileSize : Max file size in bytes (only discord uploads, not embeds)
+- earRapeBar : Level in dB to consider as ear rape
--- index.js
+++ index.js
@@ -1,71 +1,84 @@
 const ffmpeg = require('fluent-ffmpeg')
-const Discord = require('discord.js')
+const { Client, Intents } = require('discord.js')
+const config = require('./config.json')
 
-const bot = new Discord.Client()
-const botToken = "YOUR TOKEN GOES HERE"
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const checkedVideos = {}
 
-const maxFileSize = 10000000 //In bytes
-const earRapeBar = -7.0 //in dB
-const message = "Warning! This video contains very loud sounds!"
-const color = [255, 75, 75]
-
-var checkedVideos = {}
-
-function checkVideo(url) {
-    return new Promise((resolve) => {
-        ffmpeg({ source: url })
-            .withAudioFilter('volumedetect')
-            .addOption('-f', 'null')
-            .noVideo()
-            .on('start', function (ffmpegCommand) {
-                console.log('Output the ffmpeg command', ffmpegCommand);
-            })
-            .on('end', function (stdout, stderr) {
-                console.log(parseFloat(stderr.match(/(?<=max_volume: )(.*)(?= dB)/gm)[0]), parseFloat(stderr.match(/(?<=mean_volume: )(.*)(?= dB)/gm)[0]))
-                resolve([parseFloat(stderr.match(/(?<=max_volume: )(.*)(?= dB)/gm)[0]), parseFloat(stderr.match(/(?<=mean_volume: )(.*)(?= dB)/gm)[0])])
-            })
-            .output("no")
-            .run()
-    })
+function checkVideo (url) {
+  return new Promise((resolve) => {
+    ffmpeg({ source: url })
+      .withAudioFilter('volumedetect')
+      .addOption('-f', 'null')
+      .noVideo()
+      .on('error', (err) => {
+        console.log('Error with ffmpeg:', err)
+      })
+      .on('start', (ffmpegCommand) => {
+        console.log('Command:', ffmpegCommand)
+      })
+      .on('end', (stdout, stderr) => {
+        console.log(
+          parseFloat(stderr.match(/(?<=max_volume: )(.*)(?= dB)/gm)[0]),
+          parseFloat(stderr.match(/(?<=mean_volume: )(.*)(?= dB)/gm)[0])
+        )
+        resolve([
+          parseFloat(stderr.match(/(?<=max_volume: )(.*)(?= dB)/gm)[0]),
+          parseFloat(stderr.match(/(?<=mean_volume: )(.*)(?= dB)/gm)[0])
+        ])
+      })
+      .output('no')
+      .run()
+  })
 }
 
-function respond(msg) {
-    embed = new Discord.MessageEmbed()
-        .setTitle(message)
-        .setFooter(msg.member.displayName, msg.member.user.avatarURL())
-        .setColor(color)
-    msg.channel.send(embed)
+function respond (msg) {
+  console.log('Is loud!\n')
+  msg.react('📢')
 }
 
-bot.on('message', (msg) => {
-    if (msg.attachments.array()[0] !== undefined) {
-        if (msg.attachments.array()[0].url.match(/(.webm)$|(.mov)$|(.mp4)$|(.avi)$/gm) !== null) {
-            if (msg.attachments.array()[0].size < maxFileSize) {
-                checkVideo(msg.attachments.array()[0].url).then((val) => {
-                    if (val[0] === 0 && val[1] > earRapeBar) {
-                        respond(msg)
-                    }
-                })
+bot.on('messageCreate', (msg) => {
+  const [attachment] = msg.attachments.values()
+
+  if (attachment !== undefined) {
+    if (attachment.url.match(/(.webm)$|(.mov)$|(.mp4)$|(.avi)$/gm) !== null) {
+      if (attachment.size < config.maxFileSize) {
+        checkVideo(attachment.url)
+          .then((val) => {
+            if (val[0] === 0 && val[1] > config.earRapeBar) {
+              respond(msg)
             }
-        }
+          })
+      }
     }
-    for (var i = 0; i < msg.embeds.length; i++) {
-        if (msg.embeds[i].type === 'video') {
-            var link = msg.embeds[i].url
-            if (checkedVideos[link] === undefined) {
-                checkVideo(link).then((val, avg) => {
-                    if (val[0] === 0 && val[1] > earRapeBar) {
-                        respond(msg)
-                        checkedVideos[link] = true
-                    } else {
-                        checkedVideos[link] = false
-                    }
-                })
-            } else if (checkedVideos[link]) {
-                respond(msg)
+  }
+
+  for (let i = 0; i < msg.embeds.length; i++) {
+    const video = msg.embeds[i].video
+
+    if (video !== undefined) {
+      // Special cases
+      if (video.url.includes('clips.twitch.tv')) {
+        console.log('Ignored twitch clip\n')
+      } else if (checkedVideos[video.url] === undefined) {
+        checkVideo(video.url)
+          .then((val) => {
+            if (val[0] === 0 && val[1] > config.earRapeBar) {
+              respond(msg)
+              checkedVideos[video.url] = true
+            } else {
+              checkedVideos[video.url] = false
             }
-        }
+          })
+      } else if (checkedVideos[video.url]) {
+        respond(msg)
+      }
     }
+  }
 })
 
-bot.login(botToken)
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`)
+})
+
+bot.login(config.botToken)
